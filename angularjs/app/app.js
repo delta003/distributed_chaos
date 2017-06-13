1bd74da58bda1391da4e8119a5a84cd3e88e86eb
dragan224 <dmarkovic13@raf.edu.rs>
2017-06-13 16:38:50 +0200

    new job gui

--- angularjs/app/app.js
+++ angularjs/app/app.js
@@ -1,32 +1,19 @@
-(function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    angular
-        .module('app', ['ngRoute', 'ngCookies'])
-        .config(config)
-        .run(run);
+  angular.module('app', ['ngRoute', 'ngCookies']).config(config).run(run);
 
-    config.$inject = ['$routeProvider', '$locationProvider'];
-    function config($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                controller: 'HomeController',
-                templateUrl: 'view/home.html',
-                controllerAs: 'vm'
-            })
-            .when('/job/:jobid', {
-                controller: 'JobController',
-                templateUrl: 'view/job.html',
-                controllerAs: 'vm'
-            })
-            .otherwise({ redirectTo: '/' });
+  config.$inject = ['$routeProvider', '$locationProvider'];
+  function config($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {controller: 'HomeController', templateUrl: 'view/home.html', controllerAs: 'vm'})
+        .when('/job/:jobid', {controller: 'JobController', templateUrl: 'view/job.html', controllerAs: 'vm'})
+        .when('/new/', {controller: 'NewJobController', templateUrl: '/view/new.html', controllerAs: 'vm'})
+        .otherwise({redirectTo: '/'});
 
-        // use the HTML5 History API
-        //$locationProvider.html5Mode(true);
-    }
+    // use the HTML5 History API
+    //$locationProvider.html5Mode(true);
+  }
 
-    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
-    function run($rootScope, $location, $cookieStore, $http) {
-
-    }
+  run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+  function run($rootScope, $location, $cookieStore, $http) {}
 })();
