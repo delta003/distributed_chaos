d4eb34caa5c026b18d565c2df1f54dacd427ecff
Lazar Milenkovic <milenkovic.lazar@gmail.com>
2017-06-14 23:00:54 +0200

    Small change

--- python/api/bootstrap_api.py
+++ python/api/bootstrap_api.py
@@ -10,7 +10,7 @@ app.debug = True
 # visualization
 @app.route('/', methods=['POST', 'GET'])
 def default_response():
-    return send_from_directory('../angularjs/', filename='')
+    return send_from_directory('../angularjs/', filename='app/index.html')
 
 
 # bootstrap API
